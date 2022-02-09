ae30699ca15a9305baecb0f0d30b612dafe01c14
Tiago Santos <tisantos@c1r10s6.42lisboa.com>
2022-02-09 23:45:56 +0000

    starting map

--- ft_containers/includes/map.hpp
+++ ft_containers/includes/map.hpp
@@ -10,6 +10,18 @@ namespace ft {
 template < class Key, class T, class Compare = std::less<Key>,
 		   class Alloc = std::allocator<ft::pair<const Key,T> > >
 class map {
+		/****************/
+		/* Member Types */
+
+		typedef Key									key_type;						
+		typedef T									mapped_type;
+		typedef ft::pair<key_type, mapped_type>		value_type;
+		typedef Compare								key_compare;
+		typedef Alloc   							allocator_type;
+
+		class value_compare : std::binary_function<value_type, value_type, bool> {
+
+		};
 
 };
 

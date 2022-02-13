6a8ac52370387b7e40aecd577c46e8f3a04aa3aa
Tiago Santos <tisantos@c1r22s1.42lisboa.com>
2022-02-13 19:23:44 +0000

    same as before commit

--- ft_containers/testes.cpp
+++ ft_containers/testes.cpp
@@ -4,10 +4,34 @@
 #include <algorithm>
 
 
+class   matrix {
+    private:
+        std::string     __name;
+        int     __row;
+        int     __col;
+        int     __init_value;
+        std::vector<std::vector<int> > __matrix;
+
+    public:
+
+        matrix(int row, int col, std::string name, double init, std::vector<std::vector<int> > matrix) : __row(0),
+                                                                                                         __col(col),
+                                                                                                         __name(name),
+                                                                                                         __init_value(init),
+                                                                                                         __matrix(matrix)
+        {};
+
+};
+
+
 int main(void) {
 	
-    std::map<int, int> myMap;
+    std::vector<std::vector<int> > myVector = {{ 0, 1, 2}}; 
+    std::vector<int>::iterator iter;    
 
-    myMap.insert (std::pair<)
+    for (iter = myVector.begin(); iter != myVector.end(); iter++) {
+        std::cout << *iter << std::endl;
+    }
 
+    matrix theMatrix(1, 2, "Hello", 5, myVector);
 }

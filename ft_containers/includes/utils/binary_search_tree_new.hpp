63435a4367cf9e434025e3eba8c815cef95e774d
Tiago Santos <tisantos@c1r19s6.42lisboa.com>
2022-02-20 21:56:31 +0000

    binary_search_tree almosto

--- ft_containers/includes/utils/binary_search_tree_new.hpp
+++ ft_containers/includes/utils/binary_search_tree_new.hpp
@@ -32,8 +32,8 @@ class BST {
         BST<T>() :  _root(nullptr)
         {};
 
-// Insert ainda incompleto
-        void insert(int key) {
+
+        void insert(int key) {                                  // ESTE NAO TA BOM AINDA
             // Invoking Insert() function
             // and passing root node and given key
             _root = insert_node(_root, key);
@@ -46,17 +46,11 @@ class BST {
             std::cout << std::endl;
         };
 
-        bool search(int key) {
+        BSTNode<T>   *search(T key) {
             // Invoking Search() operation
             // and passing root node
             BSTNode<T> *result = search_node(_root, key);
-
-            if (result == nullptr) {
-                return false;
-            } 
-            else {
-                return true;
-            }   
+            return result;
         };
 
         BSTNode<T>  *find_min() {
@@ -71,19 +65,19 @@ class BST {
             return (successor_node(search(key)));  
         };
 
-        BSTNode<T>  *predecessor(int key) {
+        BSTNode<T>  *predecessor(T key) {
             return (predecessor_node(search(key)));
         };
 
-        void remove(int v) {
-
+        void remove(T key) {
+            _root = remove_node(_root, key);
         };
 
     protected:
         /********************/
 		/* Helper Functions */
 
-        BSTNode<T>     *insert_node(BSTNode<T> *node, int key) {
+        BSTNode<T>     *insert_node(BSTNode<T> *node, T& key) {            // GOOD - Falta size só
             // If BST doesn't exist create a new node as root
             // Or if the child has no nodes 
             if (node == nullptr) {
@@ -125,7 +119,7 @@ class BST {
 
         };
 
-        BSTNode<T>     *search_node(BSTNode<T> *node, int key) {
+        BSTNode<T>     *search_node(BSTNode<T> *node, T& key) {
             // The key is not found
             if (node == nullptr) {
                 return nullptr;
@@ -144,6 +138,7 @@ class BST {
             else {
                 return search_node(node->left, key);
             }
+            return (nullptr);
         };
 
         BSTNode<T>      *find_min_node(BSTNode<T> *node) {
@@ -171,14 +166,16 @@ class BST {
         };
 
         BSTNode<T>      *successor_node(BSTNode<T> *node) {
+            if (node == nullptr) {
+                return nullptr;
+            }
             // The successor is the minimum key value
             // of right subtree
             if (node->right != nullptr) {
                 return find_min_node(node->right);
             }
             // If no any right subtree
-            else
-            {
+            else {
                 BSTNode<T> *parentNode = node->parent;
                 BSTNode<T> *currentNode = node;
 
@@ -198,11 +195,94 @@ class BST {
         };
 
         BSTNode<T>      *predecessor_node(BSTNode<T> *node) {
+            if (node == nullptr) {
+                return nullptr;
+            }
+            // The predecessor is the maximum key value
+            // of left subtree
+            if (node->right != nullptr) {
+                return find_max_node(node->left);
+            }
+            // If no any right subtree
+            else {
+                BSTNode<T> *parentNode = node->parent;
+                BSTNode<T> *currentNode = node;
 
+                // If currentNode is not root and
+                // currentNode is its left children
+                // continue moving up
+                while ((parentNode != NULL) && (currentNode == parentNode->left)) {
+                    currentNode = parentNode;
+                    parentNode = currentNode->parent;
+                }
+
+                // If parentNode is not NULL
+                // then the key of parentNode is
+                // the predecessor of node
+                return parentNode;
+            }
         };
 
-        BSTNode<T>     *remove_node(BSTNode<T> *node, int v) {
+        BSTNode<T>     *remove_node(BSTNode<T> *node, T& key) {
+            // The given node is not found in BST
+            if (node == nullptr) {
+                return nullptr;
+            }
 
+            // Target node is found
+            if (node->key == key) {
+                BSTNode<T>    *tmp_node = node;
+                // If the node is a leaf node
+                // The node can be safely removed
+                if (node->left == nullptr && node->right == nullptr) {
+                    node = nullptr;
+                    delete tmp_node;
+                }
+                // The node have only one child at right
+                else if (node->left == nullptr && node->right != nullptr) {
+                    // The only child will be connected to
+                    // the parent's of node directly
+                    node->right->parent = node->parent;
+
+                    // Bypass node
+                    node = node->right;
+                    delete tmp_node;
+                }
+                // The node have only one child at left
+                else if (node->left != nullptr && node->right == nullptr)
+                {
+                    // The only child will be connected to
+                    // the parent's of node directly
+                    node->left->parent = node->parent;
+
+                    // Bypass node
+                    node = node->left;
+                    delete tmp_node;
+                }
+                // The node have two children (left and right)
+                else
+                {
+                    // Find successor or predecessor to avoid quarrel
+                    BSTNode<T>  *predecessorKey = predecessor_node(node);
+
+                    // Replace node's key with successor's key
+                    node->key = predecessorKey->key;
+
+                    // Delete the old successor's key
+                    node->right = remove_node(node->left, predecessorKey->key);
+                }
+            }
+            // Target node's key is smaller than
+            // the given key then search to right
+            else if (node->key < key)
+                node->right = remove_node(node->right, key);
+            // Target node's key is greater than
+            // the given key then search to left
+            else
+                node->left = remove_node(node->left, key);
+
+            // Return the updated BST
+            return node;
         };
 
     private:       
--- ft_containers/testes.cpp
+++ ft_containers/testes.cpp
@@ -24,16 +24,66 @@ int main(void) {
     std::cout << std::endl;
 
     std::cout << std::boolalpha;
-    std::cout << alo.search(5) << std::endl;
-    std::cout << alo.search(3) << std::endl;
-    std::cout << alo.search(0) << std::endl;
+    std::cout << alo.search(5) << std::endl;   // No Exists
+    std::cout << alo.search(3) << std::endl;   // No Exists
+    std::cout << alo.search(0) << std::endl;   // No Exists
+    std::cout << alo.search(95) << std::endl;  // Exists
 
     std::cout << std::endl;
 
-    std::cout << alo.find_min()->key << std::endl;
-    std::cout << alo.find_max()->key << std::endl;
+    std::cout << "min - " << alo.find_min()->key << std::endl;
+    std::cout << "max - " << alo.find_max()->key << std::endl;
 
-    aloNode = alo.successor(5);
+    std::cout << std::endl;
+
+    {
+        aloNode = alo.successor(15);
+        if (aloNode != NULL) 
+            std::cout << "alo.successor(15)    --> " << aloNode->key << std::endl;
+
+        aloNode = alo.successor(13);
+        if (aloNode != NULL) 
+            std::cout << "alo.successor(13)    --> " << aloNode->key << std::endl;
+    }
+
+    {
+        aloNode = alo.predecessor(15);
+        if (aloNode != NULL) 
+            std::cout << "alo.predecessor(15)  --> " << aloNode->key << std::endl;
+
+        aloNode = alo.predecessor(95);
+        if (aloNode != NULL) 
+            std::cout << "alo.predecessor(95)  --> " << aloNode->key << std::endl;
+    }
+
+    std::cout << std::endl;
+
+    alo.print_tree_in_order();
+
+    alo.remove(15);
+    std::cout << "remove(15)" << std::endl;
+    alo.print_tree_in_order();
+
+    alo.remove(12);
+    std::cout << "remove(12)" << std::endl;
+    alo.print_tree_in_order();
+
+    alo.remove(95);
+    std::cout << "remove(95)" << std::endl;
+    alo.print_tree_in_order();
+
+    alo.remove(13);
+    std::cout << "remove(13)" << std::endl;
+    alo.print_tree_in_order();
+
+    alo.remove(22);
+    std::cout << "remove(22)" << std::endl;
+    alo.print_tree_in_order();
+
+    alo.remove(33);
+    std::cout << "remove(33)" << std::endl;
+    alo.print_tree_in_order();
 
 
+    std::cout << std::endl;
 }
